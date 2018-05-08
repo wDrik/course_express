f92467fbcc84a7a721d6901fdae6c5040e580220
Iorgen Wildrik <w.drik@outlook.com>
2018-05-08 20:21:37 -0300

    static files

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var express = require('express');
-var http    = require('http');
-var app     = express();
-var routes  = require('./routes');
-var bodyParser = require('body-parser');
+var express     = require('express');
+var path        = require('path');
+var http        = require('http');
+var app         = express();
+var routes      = require('./routes');
+var bodyParser  = require('body-parser');
 
 app.get('/', function(req, res) {
     res.send('Hello world from express by SON');
@@ -14,12 +15,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-app.use('/hello', routes);
 
+app.use('/hello', routes);
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // http.createServer(app).listen(3000, function() {
 //     console.log('Express started');
 // });
+
 app.listen(3000, function() {
     console.log('Express started');
 });
